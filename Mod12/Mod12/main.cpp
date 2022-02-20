61b7bc57d6641c86c53f988df927388629f4474d
Anton Elkind <anton.elkind@gmail.com>
2022-02-20 23:34:23 +0200

    Add function according to HW

--- Mod12/Mod12/main.cpp
+++ Mod12/Mod12/main.cpp
@@ -1,10 +1,42 @@
 #include <iostream>
+#include "sort.h"
 
 using namespace std;
+
+bool IsKPeriodic(const string& txt, int k);
 
 int main()
 {
 	cout << "HW Module 12" << endl;
 
+	string txt = "abcabcabcabc";
+	int k = 3;
+
+	if (IsKPeriodic(txt, k))
+	{
+		cout << "Periodic" << endl;
+	}
+	else
+	{
+		cout << "Not periodic" << endl;
+	}
+
 	return 0;
+}
+
+bool IsKPeriodic(const string& txt, int k)
+{
+	if (k < 1)
+	{
+		return false;
+	}
+
+	string pat = txt.substr(0, k);
+	int found = KMPSearch(pat, txt);
+	
+	if (found * k == txt.length())
+	{
+		return true;
+	}
+	return false;
 }
